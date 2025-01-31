3a467e3a2ea3dbe230665d0b2289f5abbd40f95a
Marcus Castilhos <marcus.drummer@live.com>
2025-01-31 13:55:06 -0300

    fix: add prettier config

--- src/modules/products/use-case/create.usecase.ts
+++ src/modules/products/use-case/create.usecase.ts
@@ -1,22 +1,25 @@
-import { BadRequestException, Injectable, Logger } from "@nestjs/common";
-import { ProductsService } from "../products.service";
-import { CreateProductsDto } from "../dto/create-products.dto";
-import { CompaniesService } from "src/modules/companies/companies.service";
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { ProductsService } from '../products.service';
+import { CreateProductsDto } from '../dto/create-products.dto';
+import { CompaniesService } from 'src/modules/companies/companies.service';
 
 @Injectable()
 export class CreateUseCase {
-    logger: Logger;
+  logger: Logger;
   constructor(
-    private readonly productsService: ProductsService, 
-    private readonly companiesService: CompaniesService) {
-      this.logger = new Logger(CreateUseCase.name);
+    private readonly productsService: ProductsService,
+    private readonly companiesService: CompaniesService,
+  ) {
+    this.logger = new Logger(CreateUseCase.name);
   }
 
   async execute(createProductsDto: CreateProductsDto) {
     try {
       this.logger.log('createProductsDto', createProductsDto);
 
-      const foundCompany = await this.companiesService.findById(createProductsDto.companyId);
+      const foundCompany = await this.companiesService.findById(
+        createProductsDto.companyId,
+      );
 
       if (!foundCompany) {
         throw new BadRequestException('Company not found');
