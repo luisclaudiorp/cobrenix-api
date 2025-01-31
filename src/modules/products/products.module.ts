9155a40357c4a7a2923ccf8ee0d15f8db405c22c
Marcus Castilhos <marcus.drummer@live.com>
2025-01-31 13:19:56 -0300

    fix: adjusted validation method to create.

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -7,11 +7,13 @@ import { FindAllUseCase } from "./use-case/find-all.usecase";
 import { CreateUseCase } from "./use-case/create.usecase";
 import { UpdateUseCase } from "./use-case/update.usecase";
 import { DeleteUseCase } from "./use-case/delete.usecase";
+import { CompaniesService } from "../companies/companies.service";
+import { CompaniesRepository } from "../companies/repository/companies.respository";
 
 @Module({
     imports: [SharedModule],
     controllers: [ProductsController],
-    providers: [ProductsService, ProductsRepository, FindAllUseCase, CreateUseCase, UpdateUseCase, DeleteUseCase],
+    providers: [ProductsService, ProductsRepository, FindAllUseCase, CreateUseCase, UpdateUseCase, DeleteUseCase, CompaniesService, CompaniesRepository],
     exports: [],
 })
 export class ProductsModule {}
--- src/modules/products/use-case/create.usecase.ts
+++ src/modules/products/use-case/create.usecase.ts
@@ -1,19 +1,22 @@
 import { BadRequestException, Injectable, Logger } from "@nestjs/common";
 import { ProductsService } from "../products.service";
 import { CreateProductsDto } from "../dto/create-products.dto";
+import { CompaniesService } from "src/modules/companies/companies.service";
 
 @Injectable()
 export class CreateUseCase {
     logger: Logger;
-  constructor(private readonly productsService: ProductsService) {
-    this.logger = new Logger(CreateUseCase.name);
+  constructor(
+    private readonly productsService: ProductsService, 
+    private readonly companiesService: CompaniesService) {
+      this.logger = new Logger(CreateUseCase.name);
   }
 
   async execute(createProductsDto: CreateProductsDto) {
     try {
       this.logger.log('createProductsDto', createProductsDto);
 
-      const foundCompany = await this.productsService.findById(createProductsDto.companyId);
+      const foundCompany = await this.companiesService.findById(createProductsDto.companyId);
 
       if (!foundCompany) {
         throw new BadRequestException('Company not found');
